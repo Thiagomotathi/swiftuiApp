eb41a6884ad39a85593aff1023efe40b5b138d8f
Thiago_mota _program <128191337+Thiagomotathi@users.noreply.github.com>
2024-04-22 17:05:29 -0300

    Initial commit

---
+++ swiftuiApp/swiftuiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiAppApp.swift
+//  swiftuiApp
+//
+//  Created by User on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
