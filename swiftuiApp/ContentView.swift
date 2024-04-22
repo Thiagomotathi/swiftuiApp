0ef33cc788a4506da6296c9180a817d38e4aa3c9
Thiago_mota _program <128191337+Thiagomotathi@users.noreply.github.com>
2024-04-22 17:40:07 -0300

    mudei a fonte para bold e mudei o sf

--- swiftuiApp/ContentView.swift
+++ swiftuiApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "shared.with.you")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Thiago!").bold()
         }
         .padding()
     }
